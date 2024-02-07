d7e64049a9023556bfbd0d434ae97c70d4fd33ff
Jwahn1 <javierwith@gmail.com>
2024-02-07 13:58:39 -0400

    first test unit for add() has been added

--- test/MatrixTest.java
+++ test/MatrixTest.java
@@ -22,6 +22,45 @@ class MatrixTest {
 
     @Test
     void add() {
+        Matrix current = new Matrix(3,3);
+        Matrix b = new Matrix(3,3);
+        Matrix res = new Matrix(3,3);
+        for(int k = -10; k<11 ;k++){
+
+            //add values to b matrix
+            for(int i = 1 ; i< b.getHeight(); i++){
+                for(int j = 1 ; j< b.getWidth(); j++){
+                    b.setElem(i,j,k);
+                }
+            }
+
+            //add values to current matrix
+            for(int i = 1 ; i< b.getHeight(); i++){
+                for(int j = 1 ; j< b.getWidth(); j++){
+                    current.setElem(i,j,k);
+                }
+            }
+
+            res = current.add(b,res);
+
+            double expected = k+k;
+            assert res != null : "the addition has failed";
+            //see if the values in res are correct as expected
+            for(int i = 1 ; i< b.getHeight(); i++){
+                for(int j = 1 ; j< b.getWidth(); j++){
+                    assert res.getElem(i,j) == expected : "the addition has failed";
+                }
+            }
+        }
+
+        b = null;
+        for(int i = 1 ; i< current.getHeight(); i++){
+            for(int j = 1 ; j< current.getWidth(); j++){
+                current.setElem(i,j,1);
+            }
+        }
+        res = current.add(b,res);
+        assert res == null : "add() function does not return null when given incorrect inputs";
     }
 
     @Test
